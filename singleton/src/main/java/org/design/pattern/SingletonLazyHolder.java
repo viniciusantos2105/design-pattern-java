e3b2d30412e9d9b2c3278ddc90d8f25e3e41522d
Vinicius Jesus <92672912+viniciusantos2105@users.noreply.github.com>
2023-11-07 20:09:13 -0300

    feat(Singleton): adicionando padrão singleton

---
+++ singleton/src/main/java/org/design/pattern/SingletonLazyHolder.java
@@ -0,0 +1,17 @@
+package org.design.pattern;
+//Singleton "Lazy Holder"
+
+public class SingletonLazyHolder {
+
+    private static class InstanceHolder{
+        public static SingletonLazyHolder instancia = new SingletonLazyHolder();
+    }
+
+    private SingletonLazyHolder(){
+        super();
+    }
+
+    public static SingletonLazyHolder getInstancia(){
+        return InstanceHolder.instancia;
+    }
+}
