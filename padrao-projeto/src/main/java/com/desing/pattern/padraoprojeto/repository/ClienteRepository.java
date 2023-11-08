9df06f702958049bd0c5ca7c1d7a026ab048de0d
Vinicius Jesus <92672912+viniciusantos2105@users.noreply.github.com>
2023-11-08 16:52:00 -0300

    desbugando

--- padrao-projeto/src/main/java/com/desing/pattern/padraoprojeto/repository/ClienteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.desing.pattern.padraoprojeto.repository;
-
-import com.desing.pattern.padraoprojeto.models.Cliente;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ClienteRepository extends CrudRepository<Cliente, Long> {
-}
--- padrao-projeto/src/main/java/com/desing/pattern/padraoprojeto/repository/EnderecoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.desing.pattern.padraoprojeto.repository;
-
-import com.desing.pattern.padraoprojeto.models.Endereco;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EnderecoRepository extends CrudRepository<Endereco, String> {
-}
